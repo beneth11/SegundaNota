e81bd5c148e609f2cd09af9c84bcfc24b7bc3829
beneth11 <141766881+beneth11@users.noreply.github.com>
2023-11-03 14:33:53 -0300

    Add files via upload

---
+++ Jogo01/src/jogo01/Jogo01.java
@@ -0,0 +1,78 @@
+package jogo01;
+
+import java.util.Scanner;
+
+public class Jogo01 {
+    public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int v1,v2;
+		
+		System.out.println("Escolha entre 0, 1 e 2 , sendo : ");
+		System.out.println("Pedra = 0 ");
+		System.out.println("Papel = 1  ");
+		System.out.println("Tesoura = 2");
+		
+		System.out.println("Jogador 1: ");
+		v1 = sc.nextInt();
+		
+		System.out.println("Jogador 2: ");
+		v2 = sc.nextInt();
+		
+		switch (v1){
+			case 0: 
+				switch (v2){
+					case 0:
+						System.out.println("Empate");
+						break;
+					case 1:
+						System.out.println("Jogador 2 ganhou");
+						break;
+					case 2:
+						System.out.println("Jogador 1 ganhou");
+						break;
+						default: 
+							System.out.println ("Selecione corretamente.");
+				} break;
+			case 1:
+				switch(v2) {
+					case 0: 
+						System.out.println("Jogador 1 ganhou");
+						break;
+					case 1:
+						System.out.println(" Empate");
+						break;
+					case 2:
+						System.out.println("Jogador 2 ganhou");
+						break;
+					default: 
+						System.out.println ("Selecione corretamente.");
+				} break;
+			case 2:
+				switch(v2) {
+						case 0:
+							System.out.println("Jogador 2 ganhou");
+							break;
+						case 1:
+							System.out.println("Jogador 1 ganhou");
+							break;
+						case 2:
+							System.out.println("Empate");
+							break;
+						default: 
+				            System.out.println ("Selecione corretamente.");
+				} break;
+			default: 
+				System.out.println ("Selecione corretamente.");
+		}
+	}
+
+	
+
+}
+    
+
+    
+ 
+	
+	
